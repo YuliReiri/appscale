e039938205a411459bd0642ba2bc68f29feca05e
Navraj Chohan <nlake44@gmail.com>
2014-11-07 15:33:21 -0600

    fixed path issue

--- AppServer_Java/dev_appserver.sh
+++ AppServer_Java/dev_appserver.sh
@@ -7,7 +7,7 @@
 [ -z "${DEBUG}" ] || set -x  # trace if $DEBUG env. var. is non-zero
 SDK_BIN="`dirname "$0" | sed -e "s#^\\([^/]\\)#${PWD}/\\1#"`" # sed makes absolute
 SDK_LIB="$SDK_BIN/../lib"
-JAR_FILE="$SDK_LIB/appengine-tools-api.jar:$SDK_LIB/appengine-api-1.0-sdk-1.8.0.jar:$SDK_LIB/appscale-smtp.jar:$SDK_LIB/appengine-api-labs-1.8.0.jar:$SDK_LIB/appscale-activation.jar:$SDK_LIB/jsr107cache-1.1.jar:$SDK_LIB/appengine-jsr107cache-1.8.0.jar:$SDK_LIB/appscale-mail.jar:$SDK_LIB/jstl-1.2.jar"
+JAR_FILE="$SDK_LIB/appengine-tools-api.jar:$SDK_LIB/user/appengine-api-1.0-sdk-1.8.0.jar:$SDK_LIB/impl/appscale-smtp.jar:$SDK_LIB/impl/appengine-api-labs-1.8.0.jar:$SDK_LIB/impl/appscale-activation.jar:$SDK_LIB/user/jsr107cache-1.1.jar:$SDK_LIB/user/appengine-jsr107cache-1.8.0.jar:$SDK_LIB/impl/appscale-mail.jar"
 
 if [ ! -e "$SDK_LIB/appengine-tools-api.jar" ]; then
     echo "Repacked jar files not found"
